ca317473859277e58b6320c9d8669f0f819d65a8
Joshua Hess <jhess@hbs.net>
2019-10-11 09:54:47 -0500

    Fixed the issues with the  .min extension

--- ColorPickerFormComponent/Properties/AssemblyInfo.cs
+++ ColorPickerFormComponent/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using CMS;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("12.29.4")]
-[assembly: AssemblyFileVersion("1.0.4")]
+[assembly: AssemblyVersion("12.29.5")]
+[assembly: AssemblyFileVersion("1.0.5")]
